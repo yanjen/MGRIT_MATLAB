f721574e507a548aec4db3b878a87fd13770af12
yanjen <yanjen224@gmail.com>
2019-05-17 15:42:43 +0900

    Finish forward eular time marching

---
+++ 1D_heat/explicit_time_marching.m
@@ -0,0 +1,14 @@
+function T = explicit_time_marching(T, Q, h, dt)
+
+l = length(T);
+T = [T, T(l-1)];
+dTdt = zeros(1, l);
+
+for i = 1:l-1
+    dTdt(i+1) = (T(i) - 2*T(i+1) + T(i+2))/h^2;
+end
+dTdt(l) = dTdt(l) + Q;
+
+T = T(1:l) + dTdt * dt;
+
+end
