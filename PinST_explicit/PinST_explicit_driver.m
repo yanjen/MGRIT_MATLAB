cd9f43f083414191f6f1fb226f5a1515443de5d3
yanjen <yanjen224@gmail.com>
2019-11-20 17:09:46 +0900

    Fix PinST code such that it performs the new algorithm (multilevel parareal)

--- PinST_explicit/PinST_explicit_driver.m
+++ PinST_explicit/PinST_explicit_driver.m
@@ -5,40 +5,36 @@ dt = 1;
 Max = 300;
 x = 0:h:Max;
 l = length(x);
-N = 60;
+N = 64;
 Y = zeros(1, l);
 for i = 1:length(x)
     if x(i) > 49 && x(i) < 111
         Y(i) = 100*sin(pi*(x(i)-50)/60);
     end
 end
-Y = repmat(Y,1,N);
 fc_ratio = 2;
 level = 4;
 num_processors = 4;
+Y = repmat(Y,1,num_processors + 1);
 
-Z = reshape(Y, l, N);
+Z = reshape(Y, l, num_processors + 1);
 surf(Z)
-pause(1);
+pause(0.5);
 
 %% Initialization
-Y = PinST_initialization(Y, h, dt, N, fc_ratio, level);
-Z = reshape(Y, l, N);
+Y = PinST_initialization(Y, h, dt, N, num_processors, fc_ratio, level);
+Z = reshape(Y, l, num_processors + 1);
 surf(Z)
-pause(1);
+pause(0.5);
 
+iter_num = 2;
 for i = level - 1 : -1 : 1
-    Y = PinST_relaxation(Y, h, dt, N, fc_ratio, i, num_processors);
-    Z = reshape(Y, l, N);
-    surf(Z)
-    pause(1);
-end
-
-for j = 1:2
-    Y = PinST_relaxation(Y, h, dt, N, fc_ratio, 1, num_processors);
-    Z = reshape(Y, l, N);
-    surf(Z)
-    pause(1)
+    for j = 1:iter_num
+        Y = PinST_relaxation(Y, h, dt, N, fc_ratio, i, level, num_processors);
+        Z = reshape(Y, l, num_processors + 1);
+        surf(Z)
+        pause(0.5);
+    end
 end
 
 end
--- PinST_explicit/PinST_relaxation.m
+++ PinST_explicit/PinST_relaxation.m
@@ -1,35 +1,48 @@
-function Y = PinST_relaxation(Y, h, dt, N, fc_ratio, level, num_processors)
+function Y = PinST_relaxation(Y, h, dt, N, fc_ratio, level, total_level, nprocs)
 
-ratio = fc_ratio ^ level;
-h = h * ratio;
-dt = dt * ratio;
+coarse_ratio = fc_ratio ^ total_level;
+fine_ratio = fc_ratio ^ level;
+h_coarse = h * coarse_ratio;
+h_fine = h * fine_ratio;
+dt_coarse = dt * coarse_ratio;
+dt_fine = dt * fine_ratio;
 
-L = length(Y)/N;
-l = (L - 1) / ratio + 1;
-
-n = floor((N - 1) / ratio);
-npn = ceil(n / num_processors);
+L = length(Y)/(nprocs + 1);
 
 Z = Y;
-last_proc_values = zeros(1,l);
-for proc = 1:num_processors
-    if (proc - 1) * npn + 1 > n
-        break
+%% Parallel Part
+for proc = 1:nprocs
+    n_start = floor(N * (proc - 1) / nprocs) + 1;
+    n_start = floor((n_start - 1) / coarse_ratio) * coarse_ratio + 1;
+    n_end = floor((N * proc) / nprocs) + 1;
+    if proc ~= nprocs
+        n_end = floor((n_end - 1) / coarse_ratio) * coarse_ratio + 1;
     end
-    for i = (proc - 1) * npn + 1 : min(proc * npn, n)
-        last_time_step = (i - 1) * ratio + 1;
-        if i == (proc - 1) * npn + 1
-            last_values = Z((last_time_step - 1) * L + 1 : ratio : last_time_step * L);
-        else
-            last_values = Y((last_time_step - 1) * L + 1 : ratio : last_time_step * L);
-        end
-        next_values = Lax_Wendroff(last_values, h, dt);
-        next_values = next_values + last_proc_values;
-        for j = i * ratio + 1 : min((i + 1) * ratio, N)
-            Y((j - 1) * L + 1 : j * L) = interp1(0:h:300, next_values, 0:h/ratio:300);
-        end
+    y_fine = interp1(0:h:300, Z((proc - 1) * L + 1:proc * L), 0:h_fine:300);
+    for j = 1:(n_end - n_start)/fine_ratio
+        y_fine = Lax_Wendroff(y_fine, h_fine, dt_fine);
     end
-    last_proc_values = next_values - Y((i * ratio + 1) * L + 1 : ratio : (i * ratio + 2) * L);
+    y_coarse = interp1(0:h:300, Z((proc - 1) * L + 1:proc * L), 0:h_coarse:300);
+    for j = 1:(n_end - n_start)/coarse_ratio
+        y_coarse = Lax_Wendroff(y_coarse, h_coarse, dt_coarse);
+    end
+    Y(proc * L + 1:(proc + 1) * L) = interp1(0:h_fine:300, y_fine, 0:h:300) - interp1(0:h_coarse:300, y_coarse, 0:h:300);
+end
+
+%% Sequential Part
+y = interp1(0:h:300, Y(1:L), 0:h_coarse:300);
+for proc = 1:nprocs
+    n_start = floor(N * (proc - 1) / nprocs) + 1;
+    n_start = floor((n_start - 1) / coarse_ratio) * coarse_ratio + 1;
+    n_end = floor((N * proc) / nprocs) + 1;
+    if proc ~= nprocs
+        n_end = floor((n_end - 1) / coarse_ratio) * coarse_ratio + 1;
+    end
+    for j = 1:(n_end - n_start)/coarse_ratio
+        y = Lax_Wendroff(y, h_coarse, dt_coarse);
+    end
+    Y(proc * L + 1:(proc + 1) * L) = Y(proc * L + 1:(proc + 1) * L) + interp1(0:h_coarse:300, y, 0:h:300);
+    y = interp1(0:h:300, Y(proc * L + 1:(proc + 1) * L), 0:h_coarse:300);
 end
 
 end
