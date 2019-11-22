9151cb0f016f48f46d0cd3bdb0e2f7a377e9cb7a
yanjen <yanjen224@gmail.com>
2019-11-22 17:36:02 +0900

    Update some variables for C++ code testing

--- PinST_explicit/PinST_explicit_driver.m
+++ PinST_explicit/PinST_explicit_driver.m
@@ -1,11 +1,11 @@
-function PinST_explicit_driver()
+function Output = PinST_explicit_driver()
 %% Parameters
 h = 1.25;
 dt = 1;
 Max = 300;
 x = 0:h:Max;
 l = length(x);
-N = 64;
+N = 128;
 Y = zeros(1, l);
 for i = 1:length(x)
     if x(i) > 49 && x(i) < 111
@@ -13,8 +13,8 @@ for i = 1:length(x)
     end
 end
 fc_ratio = 2;
-level = 4;
-num_processors = 4;
+level = 3;
+num_processors = 8;
 Y = repmat(Y,1,num_processors + 1);
 
 Z = reshape(Y, l, num_processors + 1);
@@ -27,8 +27,8 @@ Z = reshape(Y, l, num_processors + 1);
 surf(Z)
 pause(0.5);
 
-iter_num = 2;
-for i = level - 1 : -1 : 1
+iter_num = 1;
+for i = level - 1 : -1 : 0
     for j = 1:iter_num
         Y = PinST_relaxation(Y, h, dt, N, fc_ratio, i, level, num_processors);
         Z = reshape(Y, l, num_processors + 1);
@@ -36,5 +36,7 @@ for i = level - 1 : -1 : 1
         pause(0.5);
     end
 end
+
+Output = Z(:,num_processors + 1)';
 
 end
